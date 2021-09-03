c1f77e159cd3345dff1ae1813c20ce50f68842b7
Thallys Silva <thallysfreed@gmail.com>
2021-09-02 23:52:43 -0300

    Criando classe funcionário

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,15 @@
 fun main() {
-    testes()
+    //testes()
+    val func = Funcionario(
+        "Teste funcionário",
+        "12345678900",
+        1000.0
+    )
+
+    println("Nome: ${func.nome}")
+    println("Salário: ${func.salario}")
+    println("Cpf: ${func.cpf}")
+    println("Bonificação: ${func.bonificacao()}")
 }
 
 
